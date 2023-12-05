04248b37c403442ec8f96b1e29f7e1c6f109627d
cushybuy <adekola@paysnug.com>
2023-12-05 09:34:41 +0100

    better

--- src/components/pages/BankingLeftComponent.jsx
+++ src/components/pages/BankingLeftComponent.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import img1 from "../../images/1.png";
 import arrowright from "../../images/arrowright.png"
 import arrowleft from "../../images/arrowleft.png"
 import arrowup from "../../images/arrowup.png"
